b1f62ece9342322b09370aede23d0eb324a96884
trouscaillon <andrew.r.allred@gmail.com>
2020-05-16 17:09:50 -0400

    first homepage

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('hello world!'))
+//app.get('/', (req, res) => res.send('hello world!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+app.use(express.static('public'))
