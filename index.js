312379888bfdc0269b6a7003b64194b3dbfa053d
andrewallred <andrewrallred@gmail.com>
2020-08-11 22:26:20 -0400

    using parcel to minify css + html

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
-app.use(express.static('public'))
+app.use(express.static('dist'))
