38c646c7e952405cb356a451d06a012ccef66756
Allred <andrewrallred@gmail.com>
2020-08-25 15:08:42 -0400

    deleting some hello world cruft

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
-//app.get('/', (req, res) => res.send('hello world!'))
-
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
 
 app.use(express.static('./dist'))
